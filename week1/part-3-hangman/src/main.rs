2793e42a8fd29d2566eeadef0030bca671523ed7
Chasing1020 <chasing1020@gmail.com>
2022-05-12 14:12:12 +0800

    modify part-3-hangman

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -14,10 +14,10 @@
 // more in depth in the coming lectures.
 extern crate rand;
 use rand::Rng;
-use std::collections::HashSet;
 use std::fs;
 use std::io;
 use std::io::Write;
+use std::iter;
 
 const NUM_INCORRECT_GUESSES: u32 = 5;
 const WORDS_PATH: &str = "words.txt";
@@ -29,19 +29,7 @@ fn pick_a_random_word() -> String {
 }
 
 fn vec_to_string(v: &Vec<char>) -> String {
-    let mut s = String::new();
-    for &c in v.iter() {
-        s.push(c);
-    }
-    s
-}
-
-fn set_to_string(map: &HashSet<char>) -> String {
-    let mut s = String::new();
-    for &c in map.iter() {
-        s.push(c);
-    }
-    s
+    v.into_iter().collect()
 }
 
 fn main() {
@@ -56,45 +44,50 @@ fn main() {
     // Your code here! :)
     println!("Welcome to CS110L Hangman!");
     let mut guess_times = 5;
-    let mut word_so_far = vec![];
-    for i in 0..secret_word_chars.len() {
-        word_so_far.push('-');
-    }
+    let mut word_so_far = iter::repeat('-').take(secret_word_chars.len()).collect();
 
-    let mut guessed_letters = HashSet::new();
+    let mut guessed_letters = vec![];
     while guess_times > 0 {
         println!("The word so far is {}", vec_to_string(&word_so_far));
-        println!("You have guessed the following letters: {}", set_to_string(&guessed_letters));
+        println!(
+            "You have guessed the following letters: {}",
+            vec_to_string(&guessed_letters)
+        );
         println!("You have {} guesses left", guess_times);
         print!("Please guess a letter: ");
         io::stdout().flush().expect("Error flushing stdout.");
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("Error reading line.");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line.");
         if guess.trim().len() != 1 {
             println!("Please input one letter, got {}", guess);
             continue;
         }
-        let letter  = guess.trim().chars().next().unwrap();
-        guessed_letters.insert(letter);
-        
+        let letter = guess.trim().chars().next().unwrap();
+        guessed_letters.push(letter);
+
         let mut matched_letters = 0;
         let mut mismatched_letters = 0;
-        for (i, &c) in secret_word_chars.iter().enumerate(){
+        for (i, &c) in secret_word_chars.iter().enumerate() {
             if c == letter {
                 word_so_far[i] = letter;
-                matched_letters+=1;
+                matched_letters += 1;
             }
             if word_so_far[i] == '-' {
                 mismatched_letters += 1;
             }
         }
         if mismatched_letters == 0 {
-            println!("\nCongratulations you guessed the secret word: {}", vec_to_string(&word_so_far));
+            println!(
+                "\nCongratulations you guessed the secret word: {}",
+                vec_to_string(&word_so_far)
+            );
             break;
         }
         if matched_letters == 0 {
             println!("Sorry, that letter is not in the word");
-            guess_times-=1;
+            guess_times -= 1;
             if guess_times == 0 {
                 println!("\nSorry, you ran out of guesses!");
             }
