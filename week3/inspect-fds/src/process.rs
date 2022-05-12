5183064c76779c05e76785cf4ed152761dd202af
Chasing1020 <chasing1020@gmail.com>
2022-05-12 16:28:35 +0800

    finish week3 Milestone 5

--- week3/inspect-fds/src/process.rs
+++ week3/inspect-fds/src/process.rs
@@ -23,16 +23,19 @@ impl Process {
     /// information will commonly be unavailable if the process has exited. (Zombie processes
     /// still have a pid, but their resources have already been freed, including the file
     /// descriptor table.)
-    #[allow(unused)] // TODO: delete this line for Milestone 3
     pub fn list_fds(&self) -> Option<Vec<usize>> {
-        // TODO: implement for Milestone 3
-        unimplemented!();
+        let dir = format!("/proc/{}/fd", self.pid);
+        let mut fds = vec![];
+        for entry in fs::read_dir(dir).ok()? {
+            let entry = entry.ok()?;
+            fds.push(entry.file_name().into_string().unwrap().parse().unwrap());
+        }
+        return Some(fds);
     }
 
     /// This function returns a list of (fdnumber, OpenFile) tuples, if file descriptor
     /// information is available (it returns None otherwise). The information is commonly
     /// unavailable if the process has already exited.
-    #[allow(unused)] // TODO: delete this line for Milestone 4
     pub fn list_open_files(&self) -> Option<Vec<(usize, OpenFile)>> {
         let mut open_files = vec![];
         for fd in self.list_fds()? {
